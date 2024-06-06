4c19c9eab8bf5a0ae7c74e1da83511354cc80441
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-06-05 22:33:59 -0400

    add animated svg and assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Sidebar from './components/sidebar/Sidebar'
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
 
 function App() {
-
   return (
     <>
       <Sidebar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
