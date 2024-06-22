13bb15e3cdea7d0598dabe55fc04197cde0b5479
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-06-21 20:08:43 -0400

    Update project section

--- frontend/src/common/ProjectCard.jsx
+++ frontend/src/common/ProjectCard.jsx
@@ -1,14 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExpand } from '@fortawesome/free-solid-svg-icons'
+
 function ProjectCard({ src, link, projectTitle, projectDescription }) {
   return (
-    <div className="card">
-      <img src={src} className="hover" alt={`${projectTitle} logo`} />
-      <div className="card__content">
-        <p className="card__title">{projectTitle}</p>
-        <p className="card__description">{projectDescription}</p>
-        <button className="card__button">
-          <a href={link} target="_blank" >Live Demo </a>
-        </button>
-        <button className="card__button secondary">Source Code</button>
+    <div className="main">
+      <div className="card">
+        
+        <div className="card_content">
+          <div className="card_image">
+            <img src={src} alt="project" />
+          </div>
+        </div>
+
+        <div className="card_back"></div>
+      </div>
+      <div className="data">
+        <div className="img">
+          <img src={src} alt="project" />
+        </div>
+        <div className="text">
+          <div className="text_m">{projectTitle}</div>
+          <div className="text_s">{projectDescription}</div>
+        </div>
+      </div>
+      <div className="btns">
+        <div className="likes">
+          <span className="likes_text">22</span>
+        </div>
+        <div className="comments">
+          <span className="comments_text">12</span>
+        </div>
+        <div className="views">
+          <span className="views_text">332</span>
+        </div>
       </div>
     </div>
   )
